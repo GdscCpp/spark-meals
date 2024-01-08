54c2640ea8c4625711e96d2d13208f0e7f30f335
Bryan Bergo <bryan.l.bergo@gmail.com>
2024-01-08 11:10:41 -0700

    carousel ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Inter, Poppins } from "next/font/google";
+import { Inter, Open_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 import {
   FirebaseAppProvider,
@@ -10,10 +10,22 @@ import {
 import firebaseConfig from "@/config/firebase";
 import Navbar from "@/lib/navabr";
 
-const poppins = Poppins({
+export const poppins = Poppins({
   subsets: ["latin"],
   weight: "600",
   variable: "--font-serif",
+});
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-sans",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-inter",
 });
 
 export default function RootLayout({
@@ -22,12 +34,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
+    <html lang="en" className={`${poppins.variable} ${openSans.variable} ${inter.variable}`}>
+      <body>
         <FirebaseAppProvider firebaseConfig={firebaseConfig}>
           <Navbar />
           {children}
         </FirebaseAppProvider>
+       
       </body>
     </html>
   );
--- src/lib/assets/icon.tsx
+++ src/lib/assets/icon.tsx
@@ -4,16 +4,18 @@ import { IconType, iconsMap } from "./icons";
 export interface IconProps {
   type: IconType;
   className?: string;
+  width?: string;
+  height?: string;
 }
 
-export default function Icon({ type, className }: IconProps) {
+export default function Icon({ type, className, width, height }: IconProps) {
   return (
     <>
       <ReactSVG
         className={className}
         src={iconsMap[type]}
-        width={"20px"}
-        height={"20px"}
+        width={width || "20px"}
+        height={height || "20px"}
       />
     </>
   );
